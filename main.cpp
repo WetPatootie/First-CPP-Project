623305e9d6aece2e9c3cce059da950b6ba9c15e3
Unknown <jerardmichaelaquino5@gmail.com>
2018-05-20 17:51:18 +0800

    Added Lines of Code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
 using namespace std;
+void menu();
 
-std::cout << "Hello World" << '\n';
+int main() {
+  menu();
+  return 0;
+}
+
+void menu (){
+  string name;
+  string program;
+  cout << "Welcome, What Is Your Name?: ";
+  cin >> name;
+  cout << "Welcome " << name << "." << '\n';
+  cout << "Program List"  << '\n';
+  cout << "1. Calculator"  << '\n';
+  cout << "2. Unit Converter"  << '\n';
+  cout << "Select A Program: " << '\n';
+  cin >> program;
+  system("pause");
+}
